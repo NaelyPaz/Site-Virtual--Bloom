2b8e1175d435714468f2eb57e240f18865bc3238
Naely <158229713+NaelyPaz@users.noreply.github.com>
2025-02-11 08:10:19 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Qualquer funcionalidade de interação que você queira adicionar
+    console.log("Bem-vindo ao Virtual Bloom!");
+});
